e54b655356e43d2e8aee2547d34f5043552e62e7
takeshi.hirosue <takeshi.hirosue@bigtreetc.com>
2024-08-25 17:06:08 +0900

    chore: Update ReviewService to use reactive types for create and get methods

--- microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
@@ -1,11 +1,16 @@
 package se.magnus.microservices.core.review.services;
 
+import static java.util.logging.Level.FINE;
+
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import se.magnus.api.core.review.Review;
 import se.magnus.api.core.review.ReviewService;
 import se.magnus.api.exceptions.InvalidInputException;
@@ -17,6 +22,8 @@ import se.magnus.util.http.ServiceUtil;
 public class ReviewServiceImpl implements ReviewService {
   private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
 
+  private final Scheduler jobScheduler;
+
   private final ReviewRepository repository;
 
   private final ReviewMapper mapper;
@@ -25,14 +32,24 @@ public class ReviewServiceImpl implements ReviewService {
 
   @Autowired
   public ReviewServiceImpl(
-      ReviewRepository repository, ReviewMapper mapper, ServiceUtil serviceUtil) {
+      Scheduler jobScheduler,
+      ReviewRepository repository,
+      ReviewMapper mapper,
+      ServiceUtil serviceUtil) {
+    this.jobScheduler = jobScheduler;
     this.repository = repository;
     this.mapper = mapper;
     this.serviceUtil = serviceUtil;
   }
 
   @Override
-  public Review createReview(Review body) {
+  public Mono<Review> createReview(Review body) {
+    return Mono.fromCallable(() -> internalCreateReview(body))
+        .log(LOG.getName(), FINE)
+        .subscribeOn(jobScheduler);
+  }
+
+  private Review internalCreateReview(Review body) {
     try {
       ReviewEntity entity = mapper.apiToEntity(body);
       ReviewEntity newEntity = repository.save(entity);
@@ -51,23 +68,36 @@ public class ReviewServiceImpl implements ReviewService {
   }
 
   @Override
-  public List<Review> getReviews(int productId) {
-
+  public Flux<Review> getReviews(int productId) {
     if (productId < 1) {
       throw new InvalidInputException("Invalid productId: " + productId);
     }
 
+    return Mono.fromCallable(() -> internalGetReviews(productId))
+        .flatMapMany(Flux::fromIterable)
+        .log(LOG.getName(), FINE)
+        .subscribeOn(jobScheduler);
+  }
+
+  private List<Review> internalGetReviews(int productId) {
     List<ReviewEntity> entityList = repository.findByProductId(productId);
     List<Review> list = mapper.entityListToApiList(entityList);
     list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
 
-    LOG.debug("getReviews: response size: {}", list.size());
+    LOG.debug("Response size: {}", list.size());
 
     return list;
   }
 
   @Override
-  public void deleteReviews(int productId) {
+  public Mono<Void> deleteReviews(int productId) {
+    return Mono.fromRunnable(() -> internalDeleteReviews(productId))
+        .log(LOG.getName(), FINE)
+        .subscribeOn(jobScheduler)
+        .then();
+  }
+
+  private void internalDeleteReviews(int productId) {
     LOG.debug(
         "deleteReviews: tries to delete reviews for the product with productId: {}", productId);
     repository.deleteAll(repository.findByProductId(productId));
